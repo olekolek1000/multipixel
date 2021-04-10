eb53a0681af63736cf50598051b8450d1a745009
KuczaRacza <kuczaracza31@gmail.com>
2021-04-11 01:11:02 +0200

    database connection

--- src/database.hpp
+++ src/database.hpp
@@ -1,11 +1,14 @@
 #include "util/smartptr.hpp"
 #include "util/types.hpp"
+#include <cstddef>
 struct sqlite3;
 class DatabaseConnector {
 public:
 	DatabaseConnector();
+	DatabaseConnector(const char * dbpath);
 	~DatabaseConnector();
-	auto saveBytes(s32 x, s32 y, uniqdata<u8> data) -> void;
+	auto saveBytes(s32 x, s32 y, const void *data ,size_t size) -> void;
+	auto createEnteryIfNotExists(s32 x, s32 y) -> void;
 	auto loadBytes(s32 x, s32 y, bool createIfNotExits = true) -> uniqdata<u8>;
 	sqlite3 *database = nullptr;
 };
