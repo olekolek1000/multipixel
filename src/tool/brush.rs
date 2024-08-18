c0571e5c3bb1d9776930d0637d8c21447ce9ba90
Aleksander <aleksander@oo8.dev>
2024-08-18 20:18:14 +0200

    Chunk system: `set_pixels_global` (WIP)

--- src/tool/brush.rs
+++ src/tool/brush.rs
@@ -7,32 +7,20 @@ pub struct BrushShape {
 
 impl BrushShape {
 	pub fn new(size: u8, filled: bool) -> Self {
-		let capacity = size as usize * size as usize;
-		let mut data: Vec<u8> = Vec::with_capacity(capacity);
+		let data = (0..size)
+			.flat_map(|y| (0..size).map(move |x| (x, y)))
+			.map(|(x, y)| {
+				let diff_x = (size as i32 / 2) - x as i32;
+				let diff_y = (size as i32 / 2) - y as i32;
+				let distance = ((diff_x * diff_x + diff_y * diff_y) as f32).sqrt();
 
-		unsafe {
-			data.set_len(capacity);
-			// Generate circle
-
-			let center_x = (size / 2) as i32;
-			let center_y = (size / 2) as i32;
-
-			for y in 0..size {
-				for x in 0..size {
-					let diff_x = center_x - x as i32;
-					let diff_y = center_y - y as i32;
-					let distance = ((diff_x * diff_x + diff_y * diff_y) as f32).sqrt();
-					let index = y as usize * size as usize + x as usize;
-
-					if filled {
-						*data.get_unchecked_mut(index) = (distance <= (size as f32) / 2.0).into();
-					} else {
-						*data.get_unchecked_mut(index) =
-							(distance <= (size as f32) / 2.0 && distance >= (size as f32 / 2.0) - 2.0).into();
-					}
+				if filled {
+					(distance <= (size as f32) / 2.0) as u8
+				} else {
+					(distance <= (size as f32) / 2.0 && distance >= (size as f32 / 2.0) - 2.0) as u8
 				}
-			}
-		}
+			})
+			.collect::<Vec<u8>>();
 
 		Self { size, data }
 	}
