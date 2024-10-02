eb9878ef5b5cff34c78f437fc5658b13930cd951
Aleksander <aleksander@oo8.dev>
2024-10-02 22:29:28 +0200

    GET RID OF DEADLOCKS, Improve chunk I/O speed by 90000%
    (benchmarked and tested (I counted in my head))

--- src/room.rs
+++ src/room.rs
@@ -156,7 +156,7 @@ impl RoomInstance {
 	}
 
 	pub async fn cleanup(&mut self) {
-		self.chunk_system.lock().await.cleanup().await;
+		ChunkSystem::cleanup(self.chunk_system.clone()).await;
 		self.preview_system.lock().await.cleanup().await;
 		self.database.lock().await.cleanup().await;
 		self.cleaned_up = true;
