67bf57652cd4b4459ef54e2548ab6ad5fe7688ee
DiD3n <den190242@gmail.com>
2025-07-03 19:03:06 +0200

    fix: remove unused styles

--- web/src/chat/chat.module.scss.d.ts
+++
@@ -1,9 +0,0 @@
-declare const classNames: {
-  readonly panel_appearance: "panel_appearance";
-  readonly panel: "panel";
-  readonly chat_box: "chat_box";
-  readonly chat_history: "chat_history";
-  readonly panel2: "panel2";
-  readonly chat_input: "chat_input";
-};
-export default classNames;
